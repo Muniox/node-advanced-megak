fe11f7a103d1d57b3e887acc229b3007bdf84683
Muniox <muniox20@gmail.com>
2023-01-10 22:14:38 +0100

    add to d02

--- d02_node_advance_assert_test/server/test.ts
+++ d02_node_advance_assert_test/server/test.ts
@@ -1,10 +1,6 @@
 import {strict as assert} from 'assert';
 import {handlebarsHelpers} from './utils/handlebars-helpers';
 
-assert(2 === 2); //najprostszy przykład
-
-assert(handlebarsHelpers.equals(2,2), 'Numbers comparison does not work.');
-
-assert(handlebarsHelpers.equals('abc','abc'), 'String comparison does not work.');
-
-assert(handlebarsHelpers.equals(false,false), 'Bools comparison does not work.');
+//deepStrictEqual
+//sprawdza czy obiekt (wszystko jest obiektem) jest taki sam
+assert.deepStrictEqual()
